b5dc115a8e0ae4d1d16fd4092775bb98f821b354
Udit Jethva <109515569+u-d-ash@users.noreply.github.com>
2024-06-07 09:29:55 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import sync_playwright
 
 config_dict = {
-    'CF_USERNAME' : "u_d_ash_",
-    'CF_PASSWORD' : "thundercl0UDS@codeforces",
-    'AT_USERNAME' : "oye_udi_udi",
-    'AT_PASSWORD' : "thundercl0UDS@atcoder"
+    'CF_USERNAME' : "",
+    'CF_PASSWORD' : "",
+    'AT_USERNAME' : "",
+    'AT_PASSWORD' : ""
 }
